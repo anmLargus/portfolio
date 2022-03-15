0973dc08b8a5235eb278a6e4a8a4dd0e8b2be194
anmLargus <andresnm29@gmail.com>
2022-03-15 16:20:14 -0300

    add errorNOTFOUND. some components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { ErrorComponent } from './pages/error/error.component';
+
+const routes: Routes = [
+  {path: "", redirectTo: "inicio", pathMatch: "full"},
+  {path: "inicio", component: InicioComponent},
+  // otros path
+  //
+  {path: "**", component: ErrorComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TopnavbarComponent } from './components/topnavbar/topnavbar.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+
+import { ErrorComponent } from './pages/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TopnavbarComponent,
+    InicioComponent,
+    
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
