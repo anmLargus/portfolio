50e6fcdb9a138c9f19ac6c63ca96b32ca87ebdf3
anmLargus <andresnm29@gmail.com>
2022-06-13 22:56:57 -0300

    probé modal confirm en link a yurca

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Persona } from 'src/app/class/persona';
+import { ConfirmService } from 'src/app/services/confirm.service';
 import { PersonaService } from 'src/app/services/persona.service';
 
 
@@ -13,10 +14,22 @@ export class FooterComponent implements OnInit {
 
     persona: Persona = new Persona() ;
   
-  constructor( private personaService: PersonaService ) { }
+  constructor( private personaService: PersonaService , private confirmService: ConfirmService) { }
 
   ngOnInit(): void {
     this.personaService.getPersona1(1).subscribe(data => (this.persona = data) );
   } 
+  // Por si deseo usar confirmación para ir a link de yurca
+  alertaLink() {
+    this.confirmService.confirm({
+      title: 'Confirmar', 
+      message: 'Este es un link externo',
+      estilo: 'primary' //alguno de los colores de bootstrap (primary, info, danger, etc)
+    }).then((confirm) => {
+      window.location.href = "https://www.instagram.com/yurcabeer/?hl=es-la";
+    }, (cancel) => {
+        console.log("Cancelado Ir a Yurca");
+    });
+  }
 
 }
